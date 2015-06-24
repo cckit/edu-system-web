19eaa237da00072e7436d5e59fb14e26cfda1fc0
Tommy Chan <ckchan.cs@gmail.com>
2015-06-25 04:29:11 +0800

    Fix bug when front end is not connected and add disconnect message

--- web_server/server.js
+++ web_server/server.js
@@ -29,15 +29,28 @@ function configSocketIO(server) {
     var clients = {};
 
     io.on('connection', function(socket) {
+        var client_id = "";
+
+        socket.on('error', function(err) {
+            console.error(JSON.stringify(err));
+        });
+
+        socket.on('disconnect', function() {
+            console.log(client_id + ' disconnected');
+        });
+
         socket.on('whoami', function(id) {
             if (id === 'back' || id === 'front') {
                 clients[id] = socket;
+                client_id = id;
                 console.log(id + ' connected');
             }
         });
 
         socket.on('kinect-image', function(data) {
-            clients['front'].emit('kinect-image', data.toString('base64'));
+            if (clients['front'] !== undefined && data !== undefined) {
+                clients['front'].emit('kinect-image', data.toString('base64'));
+            }
         });
     });
 }
